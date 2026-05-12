058a6802be9a1e560a2a5aacbf71eb139bd90daa
abtark <tarkrimhasan@gmail.com>
2026-05-12 22:39:01 +0600

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,9 @@ import Navbar from '@/components/Navbar'
 import Hero from '@/components/Hero'
 import About from '@/components/About'
 import Services from '@/components/Services'
-import Insights from '@/components/Insights'
 import Testimonials from '@/components/Testimonials'
 import Teams from '@/components/Teams'
 import Gallery from '@/components/Gallery'
-import Careers from '@/components/Careers'
 import Contact from '@/components/Contact'
 import Footer from '@/components/Footer'
 
@@ -14,19 +12,15 @@ export default function Home() {
   return (
     <main className="bg-[#111111] min-h-screen text-white overflow-hidden selection:bg-[#00AAFF]/30 selection:text-white">
       <Navbar />
-
       <div className="flex flex-col w-full">
-        <Hero />          {/* id="home" */}
-        <About />         {/* id="about" */}
-        <Services />      {/* id="services" */}
-        <Insights />      {/* id="insights" */}
-        <Testimonials />  {/* id="testimonials" */}
-        <Teams />         {/* id="teams" */}
-        <Gallery />       {/* id="gallery" */}
-        <Careers />       {/* id="careers" */}
-        <Contact />       {/* id="contact" */}
+        <Hero />
+        <About />
+        <Services />
+        <Testimonials />
+        <Teams />
+        <Gallery />
+        <Contact />
       </div>
-
       <Footer />
     </main>
   )
