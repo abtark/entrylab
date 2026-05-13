2fd12083d2a0659263b11f0d086a2052a600e083
abtark <tarkrimhasan@gmail.com>
2026-05-13 23:45:06 +0600

    page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,15 @@
-import Contact from '../../components/Contact'
+import Navbar from '@/components/Navbar'
+import Contact from '@/components/Contact'
+import Footer from '@/components/Footer'
 
 export default function ContactPage() {
   return (
-    <main>
-      <Contact />
+    <main className="bg-[#111111] min-h-screen text-white overflow-hidden selection:bg-[#00AAFF]/30 selection:text-white">
+      <Navbar />
+      <div className="flex flex-col w-full">
+        <Contact />
+      </div>
+      <Footer />
     </main>
   )
 }
